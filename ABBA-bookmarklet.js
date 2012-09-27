ae3ff25c9ca15a1671a364f3174dfbbe90d572c2
yahelc <yahelc@gmail.com>
2012-09-27 16:57:33 -0500

    Formatting fix

--- ABBA-bookmarklet.js
+++ ABBA-bookmarklet.js
@@ -1,25 +1,24 @@
 	(function(){
-			function abba_url(data_array){
-						var url = "http://www.thumbtack.com/labs/abba/#";
-									for(var i = 0; i<data_array.length; i++){
-													url += "Variation+" + (i+1) + "=" + data_array[i] + "%2C" + data_array[++i] + "&";
-																}
-																			url += "abba%3AintervalConfidenceLevel=0.95&abba%3AuseMultipleTestCorrection=true";
-																						return url;
-																								}
+		function abba_url(data_array){
+			var url = "http://www.thumbtack.com/labs/abba/#";
+			for(var i = 0; i<data_array.length; i++){
+				url += "Variation+" + (i+1) + "=" + data_array[i] + "%2C" + data_array[++i] + "&";
+			}
+			url += "abba%3AintervalConfidenceLevel=0.95&abba%3AuseMultipleTestCorrection=true";
+			return url;
+		}
+		
+		if(window.abbaDispatchOnNext){
+			window.open(abba_url(abbaData));
+			this.removeEventListener('click', arguments.callee, true);
+		}
+		window.abbaDispatchOnNext = true;
+		window.abbaData = window.abbaData || [];
+		document.addEventListener("click", function(e){
+			var num = e.target.textContent.replace(/\D/g,'');
+			if(typeof +(num) === "number" && !isNaN(+num)){
+				abbaData.push(num);
+			}
 
-																										if(window.abbaDispatchOnNext){
-																													window.open(abba_url(abbaData));
-																																this.removeEventListener('click', arguments.callee, true);
-																																		}
-																																				window.abbaDispatchOnNext = true;
-																																						window.abbaData = window.abbaData || [];
-																																								document.addEventListener("click", function(e){
-																																											var num = e.target.textContent.replace(/\D/g,'');
-																																														if(typeof +(num) === "number" && !isNaN(+num)){
-																																																		abbaData.push(num);
-																																																					}
-
-																																																							}, true);		
-																																																								}());
-
+		}, true);		
+	}());
